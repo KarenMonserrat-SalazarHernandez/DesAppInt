2272b28ce17337209a6b77e4f60413bfafa2ad55
KarenMonserrat-SalazarHernandez <karenmonshe2@gmail.com>
2023-08-22 13:16:13 -0600

    Actividad 01

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 
 Actualización!
 
+## Actividad 1
+
+- p01-hola-mundo
+- p02-paga-trabajador
+- p03-venta-pizza
---
+++ p01-hola-mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿// Programa que envia un saludo en pantalla
+
+string amigo = "Adrian";
+string hermano = "Reynaldo";
+
+Console.WriteLine("Hola " + amigo);
+Console.WriteLine($"Hola {amigo} bienvenido a C#");
+Console.WriteLine($"\nMi amigo es {amigo} mi hermano es {hermano}");
+Console.WriteLine("Nombres en mayusculas");
+Console.WriteLine($"{amigo.ToUpper()}  {hermano.ToUpper()}");
+
+
